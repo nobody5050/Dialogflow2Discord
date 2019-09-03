f88b7328118e5882e9283a9ae8a37116207fdc1d
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2019-09-03 10:40:12 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	Dialogflow: '<Enter your Dialogflow token here>',
-	Discord: '<Enter your discord token here>'
+	Dialogflow: 'ff1588901dda4bdd80b5baa8c8674b82',
+	Discord: 'NTg1NDg3NTQyOTkzMDI3MDcy.XW6JGQ.P-43bZkUTqw6NuvylVmzh0HNxq8'
 }
