b65996bed370e50050e3684d4ea243c0e62c7d3c
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2019-09-09 11:37:07 -0500

    Emo

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	Dialogflow: 'ff1588901dda4bdd80b5baa8c8674b82',
+	Dialogflow: '27aa7ea93b3e4eac993fca9ec107da35',
 	Discord: 'NTg1NDg3NTQyOTkzMDI3MDcy.XW6JGQ.P-43bZkUTqw6NuvylVmzh0HNxq8'
 }
